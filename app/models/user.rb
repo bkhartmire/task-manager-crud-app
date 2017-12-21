21b26cddbde6b80448c99595239f5029b843fa06
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-21 13:17:48 +0000

    models

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  has_many :tasks
+  has_secure_password
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect {|user| user.slug == slug}
+  end
+  
+end
