2ba199e9119e9934244b211b12f6c01662ffda02
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-21 15:24:36 +0000

    ben ascii art

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
     if !logged_in?
       erb :index
     else
-      redirect "/users/#{@user.slug}"
+      redirect "/users/#{current_user.slug}"
     end
   end
 
