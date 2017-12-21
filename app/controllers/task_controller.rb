d10b330c08533a9ae52a10a8615162bf1226e094
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-21 14:25:39 +0000

    oops tasks, not tweets

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -4,11 +4,22 @@ require 'rack-flash'
 class TaskController < ApplicationController
   use Rack::Flash
 
-  get '/tweets/new' do
+  get '/tasks/new' do
     if logged_in?
       erb :'tasks/create_tasks'
     else
       redirect '/login'
+    end
+  end
+
+  post '/tasks' do
+    unless params[:content].empty?
+      @tweet = Tweet.create(:content => params[:content])
+      @tweet.user_id = current_user.id
+      @tweet.save
+      erb :"tweets/show_tweet"
+    else
+      redirect "/tweets/new"
     end
   end
 end
