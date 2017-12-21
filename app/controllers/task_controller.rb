f8488ba8ed10828d981c76f173185dd1dd48dfc8
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-21 14:24:52 +0000

    get /tweets/new

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -3,4 +3,12 @@ require 'rack-flash'
 
 class TaskController < ApplicationController
   use Rack::Flash
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tasks/create_tasks'
+    else
+      redirect '/login'
+    end
+  end
 end
