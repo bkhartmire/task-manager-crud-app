7337ac364af2e996d718ca7c66cf899cb84430dc
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-21 13:48:35 +0000

    usercontroller actions

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -17,8 +17,31 @@ class UserController < ApplicationController
       @user = User.create(username: params[:userame], email: params[:email], password: params[:password])
       @user.save
       session[:user_id] = @user.id
-      erb :'users/show'
+      redirect '/users/#{@user.slug}'
     end
   end
 
+  get '/login' do
+    if logged_in?
+      redirect '/users/#{@user.slug}'
+    else
+      erb :'users/login'
+    end
+
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/users/#{@user.slug}'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
 end
