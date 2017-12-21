a44e8294036fe7c1d1a31c72df1747c85ecf06f2
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-21 13:15:45 +0000

    helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,15 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+  end
 end
