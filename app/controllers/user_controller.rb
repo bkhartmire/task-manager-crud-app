7f0dc11e5c0a34a034f910f152fa964c55d56ba9
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-21 13:37:24 +0000

    get /signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,14 @@
 require './config/environment'
 
 class UserController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :'users/create_user'
+    end
+  end
+
+
 end
